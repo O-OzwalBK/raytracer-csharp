edb80d0d014fe0fae0a56d511f7d0d56753b9a1f
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2025-02-03 00:53:32 +0545

    Setup format for the ppm image file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    readonly int imageHeight = 512;
+    readonly int imageWidth = 512;
+
+    static readonly string filePath = "../raytracer/obj/";
+    static private void Main()
+    {
+        Program program = new();
+        program.WriteToFile("RaytracedImageOutput.ppm");
+    }
+
+    private void WriteToFile(string fileName)
+    {
+        using (StreamWriter imageFile = new($"{filePath}/{fileName}"))
+        {
+            imageFile.WriteLine($"P3\n{imageWidth} {imageHeight}\n255");
+        }
+    }
+}
