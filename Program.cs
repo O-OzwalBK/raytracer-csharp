de4819269e8da0a743d30fd0a324d0929471b54e
Mahakaal <59462639+O-ozwal@users.noreply.github.com>
2025-02-03 16:27:37 +0545

    Test a Red-Green gradient. Repeating values issue

--- Program.cs
+++ Program.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.IO;
-
-class Program
+internal class Program
 {
-    readonly int imageHeight = 512;
-    readonly int imageWidth = 512;
+    static readonly int imageHeight = 512;
+    static readonly int imageWidth = 512;
 
     static readonly string filePath = "../raytracer/obj/";
-    static private void Main()
+    private static void Main()
     {
-        Program program = new();
-        program.WriteToFile("RaytracedImageOutput.ppm");
+        WriteToFile("RaytracedImageOutput.ppm");
     }
 
-    private void WriteToFile(string fileName)
+    static private void WriteToFile(string fileName)
     {
-        using (StreamWriter imageFile = new($"{filePath}/{fileName}"))
+        try
         {
-            imageFile.WriteLine($"P3\n{imageWidth} {imageHeight}\n255");
+            using (StreamWriter imageFile = new($"{filePath}/{fileName}"))
+            {
+                // PPM image header
+                imageFile.WriteLine("P3");
+                imageFile.WriteLine($"{imageWidth} {imageHeight}");
+                imageFile.WriteLine("255");
+
+                // for pixel color values
+                for (int row = 0; row < imageHeight; row++)
+                {
+                    for (int column = 0; column < imageWidth; column++)
+                    {
+                        double red = (double)column / (imageWidth - 1);
+                        double green = (double)row / (imageHeight - 1);
+                        double blue = 0.0;
+
+                        int intRed = (int)(255.999 * red);
+                        int intGreen = (int)(255.999 * green);
+                        int intBlue = (int)(255.999 * blue);
+
+                        imageFile.WriteLine($"{intRed} {intGreen} {intBlue}");
+                    }
+                }
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
         }
     }
 }
